c896cf03e55a6c82f94e69c33325e99502703e59
Ivona Perko <ivona.perko@dice.hr>
2024-05-07 19:18:09 +0200

    Add initial xcode swiftui project

---
+++ Roamer/Roamer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roamer
+//
+//  Created by Ivona Perko on 07.05.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roamer/Roamer/RoamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoamerApp.swift
+//  Roamer
+//
+//  Created by Ivona Perko on 07.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct RoamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
